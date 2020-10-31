4e08ceed28b470cf25b82b6e0e60db5ebc89fd47
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-10-30 23:31:22 -0400

    commit tree 4e536d484ea9c1afc29e749a6aa0a08b6c34adc5

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+    i = 0
+    new_collection = []
+    while i < collection.length
+      new_collection << yield(collection[i])
+      i += 1
+    end
+    new_collection
+end
 
